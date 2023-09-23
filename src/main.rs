213a01df19cbd2db1ec99ac00e8819ba2998f096
0xAtticus <atticus.the.just@gmail.com>
2023-09-23 16:50:14 +0200

    blog pt1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,36 @@
+use std::ffi::CStr;
+use std::ffi::CString;
+
+use nix::sys::ptrace;
+use nix::sys::wait::waitpid;
+use nix::unistd::{fork, ForkResult};
+
 fn main() {
-    println!("Hello, world!");
+    let fork_result = unsafe { fork() }.expect("Failed to fork");
+    match fork_result {
+        ForkResult::Parent { child } => {
+            let _ = waitpid(child, None).expect("Failed to wait");
+            ptrace::syscall(child, None).expect("Failed to use PTRACE_SYSCALL");
+            loop {
+                let _ = waitpid(child, None).expect("Failed to wait");
+                let before_call_registers = ptrace::getregs(child).expect("could not get child's registers");
+                println!("Entering syscall #{}", before_call_registers.orig_rax);
+                
+                ptrace::syscall(child, None).expect("Failed to use PTRACE_SYSCALL");
+                
+                let _ = waitpid(child, None).expect("Failed to wait");
+                let after_call_registers = ptrace::getregs(child).expect("could not get child's registers");
+                println!("Syscall #{}, Result: ({}, {})", before_call_registers.orig_rax, after_call_registers.rax, after_call_registers.rdx);
+                
+                ptrace::syscall(child, None).expect("Failed to use PTRACE_SYSCALL");
+            }
+        }
+
+        ForkResult::Child => {
+            ptrace::traceme().expect("Failed to call traceme in child");
+            let path: &CStr = &CString::new("../debugee/target/release/debugee").unwrap();
+            nix::unistd::execve::<&CStr, &CStr>(path, &[], &[]).unwrap();
+            unreachable!("Execve should have replaced the program");
+        }
+    }
 }
